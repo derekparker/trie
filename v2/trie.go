534d0fa8890df04681f281ea358a653fd077cdcb
Derek Parker <parkerderek86@gmail.com>
2023-09-28 17:25:20 -0700

    refactor

--- v2/trie.go
+++ v2/trie.go
@@ -10,21 +10,21 @@ import (
 	"sync"
 )
 
-type Node[T any] struct {
+type node[T any] struct {
 	val       rune
 	path      string
 	term      bool
 	depth     int
 	meta      T
 	mask      uint64
-	parent    *Node[T]
-	children  map[rune]*Node[T]
+	parent    *node[T]
+	children  map[rune]*node[T]
 	termCount int
 }
 
 type Trie[T any] struct {
-	mu   sync.Mutex
-	root *Node[T]
+	mu   sync.RWMutex
+	root *node[T]
 	size int
 }
 
@@ -39,84 +39,86 @@ const nul = 0x0
 // New creates a new Trie with an initialized root Node.
 func New[T any]() *Trie[T] {
 	return &Trie[T]{
-		root: &Node[T]{children: make(map[rune]*Node[T]), depth: 0},
+		root: &node[T]{children: make(map[rune]*node[T]), depth: 0},
 		size: 0,
 	}
-}
-
-// Root returns the root node for the Trie.
-func (t *Trie[T]) Root() *Node[T] {
-	return t.root
 }
 
 // Add adds the key to the Trie, including meta data. Meta data
 // is stored as `interface{}` and must be type cast by
 // the caller.
-func (t *Trie[T]) Add(key string, meta T) *Node[T] {
+func (t *Trie[T]) Add(key string, meta T) *node[T] {
 	t.mu.Lock()
+	defer t.mu.Unlock()
 
 	t.size++
 	runes := []rune(key)
 	bitmask := maskruneslice(runes)
-	node := t.root
-	node.mask |= bitmask
-	node.termCount++
+	nd := t.root
+	nd.mask |= bitmask
+	nd.termCount++
 	for i := range runes {
 		r := runes[i]
 		bitmask = maskruneslice(runes[i:])
-		if n, ok := node.children[r]; ok {
-			node = n
-			node.mask |= bitmask
+		if n, ok := nd.children[r]; ok {
+			nd = n
+			nd.mask |= bitmask
 		} else {
-			node = node.NewEmptyChild(r, "", bitmask)
+			nd = nd.NewEmptyChild(r, "", bitmask)
 		}
-		node.termCount++
+		nd.termCount++
 	}
-	node = node.NewChild(nul, key, 0, meta, true)
-	t.mu.Unlock()
+	nd = nd.NewChild(nul, key, 0, meta, true)
 
-	return node
+	return nd
 }
 
 // Find finds and returns meta data associated
 // with `key`.
-func (t *Trie[T]) Find(key string) (*Node[T], bool) {
-	node := findNode(t.Root(), []rune(key))
-	if node == nil {
+func (t *Trie[T]) Find(key string) (*node[T], bool) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	nd := findNode(t.root, []rune(key))
+	if nd == nil {
 		return nil, false
 	}
 
-	node, ok := node.Children()[nul]
-	if !ok || !node.term {
+	nd, ok := nd.Children()[nul]
+	if !ok || !nd.term {
 		return nil, false
 	}
 
-	return node, true
+	return nd, true
 }
 
 func (t *Trie[T]) HasKeysWithPrefix(key string) bool {
-	node := findNode(t.Root(), []rune(key))
-	return node != nil
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	nd := findNode(t.root, []rune(key))
+	return nd != nil
 }
 
 // Remove removes a key from the trie, ensuring that
 // all bitmasks up to root are appropriately recalculated.
 func (t *Trie[T]) Remove(key string) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
 	var (
-		rs   = []rune(key)
-		node = findNode(t.Root(), []rune(key))
+		rs = []rune(key)
+		nd = findNode(t.root, []rune(key))
 	)
 
-	if node == nil {
+	if nd == nil {
 		return
 	}
 
-	t.mu.Lock()
-
 	t.size--
-	for n := node.Parent(); n != nil; n = n.Parent() {
+	for n := nd.Parent(); n != nil; n = n.Parent() {
 		if n == t.root {
-			t.root = &Node[T]{children: make(map[rune]*Node[T])}
+			t.root = &node[T]{children: make(map[rune]*node[T])}
 			break
 		}
 
@@ -125,7 +127,6 @@ func (t *Trie[T]) Remove(key string) {
 			break
 		}
 	}
-	t.mu.Unlock()
 }
 
 // Keys returns all the keys currently stored in the trie.
@@ -139,31 +140,31 @@ func (t *Trie[T]) Keys() []string {
 
 // FuzzySearch performs a fuzzy search against the keys in the trie.
 func (t *Trie[T]) FuzzySearch(pre string) []string {
-	keys := fuzzycollect(t.Root(), []rune(pre))
+	keys := fuzzycollect(t.root, []rune(pre))
 	sort.Sort(ByKeys(keys))
 	return keys
 }
 
 // PrefixSearch performs a prefix search against the keys in the trie.
 func (t *Trie[T]) PrefixSearch(pre string) []string {
-	node := findNode(t.Root(), []rune(pre))
-	if node == nil {
+	nd := findNode(t.root, []rune(pre))
+	if nd == nil {
 		return nil
 	}
 
-	return collect(node)
+	return collect(nd)
 }
 
 // NewChild creates and returns a pointer to a new child for the node.
-func (n *Node[T]) NewChild(val rune, path string, bitmask uint64, meta T, term bool) *Node[T] {
-	node := &Node[T]{
+func (n *node[T]) NewChild(val rune, path string, bitmask uint64, meta T, term bool) *node[T] {
+	node := &node[T]{
 		val:      val,
 		path:     path,
 		mask:     bitmask,
 		term:     term,
 		meta:     meta,
 		parent:   n,
-		children: make(map[rune]*Node[T]),
+		children: make(map[rune]*node[T]),
 		depth:    n.depth + 1,
 	}
 	n.children[node.val] = node
@@ -172,13 +173,13 @@ func (n *Node[T]) NewChild(val rune, path string, bitmask uint64, meta T, term b
 }
 
 // NewEmptyChild creates and returns a pointer to a new child for the node.
-func (n *Node[T]) NewEmptyChild(val rune, path string, bitmask uint64) *Node[T] {
-	node := &Node[T]{
+func (n *node[T]) NewEmptyChild(val rune, path string, bitmask uint64) *node[T] {
+	node := &node[T]{
 		val:      val,
 		path:     path,
 		mask:     bitmask,
 		parent:   n,
-		children: make(map[rune]*Node[T]),
+		children: make(map[rune]*node[T]),
 		depth:    n.depth + 1,
 	}
 	n.children[node.val] = node
@@ -186,7 +187,7 @@ func (n *Node[T]) NewEmptyChild(val rune, path string, bitmask uint64) *Node[T] 
 	return node
 }
 
-func (n *Node[T]) RemoveChild(r rune) {
+func (n *node[T]) RemoveChild(r rune) {
 	delete(n.children, r)
 	for nd := n.parent; nd != nil; nd = nd.parent {
 		nd.mask ^= nd.mask
@@ -198,51 +199,51 @@ func (n *Node[T]) RemoveChild(r rune) {
 }
 
 // Parent returns the parent of this node.
-func (n *Node[T]) Parent() *Node[T] {
+func (n *node[T]) Parent() *node[T] {
 	return n.parent
 }
 
 // Meta returns the meta information of this node.
-func (n *Node[T]) Meta() T {
+func (n *node[T]) Meta() T {
 	return n.meta
 }
 
 // Children returns the children of this node.
-func (n *Node[T]) Children() map[rune]*Node[T] {
+func (n *node[T]) Children() map[rune]*node[T] {
 	return n.children
 }
 
 // Terminating returns true if this node terminates an entry in the Trie.
-func (n *Node[T]) Terminating() bool {
+func (n *node[T]) Terminating() bool {
 	return n.term
 }
 
 // Val returns the rune value of the Node.
-func (n *Node[T]) Val() rune {
+func (n *node[T]) Val() rune {
 	return n.val
 }
 
 // Depth returns this nodes depth in the tree.
-func (n *Node[T]) Depth() int {
+func (n *node[T]) Depth() int {
 	return n.depth
 }
 
 // Mask returns a uint64 representing the current
 // mask of this node.
-func (n *Node[T]) Mask() uint64 {
+func (n *node[T]) Mask() uint64 {
 	return n.mask
 }
 
-func findNode[T any](node *Node[T], runes []rune) *Node[T] {
-	if node == nil {
+func findNode[T any](nd *node[T], runes []rune) *node[T] {
+	if nd == nil {
 		return nil
 	}
 
 	if len(runes) == 0 {
-		return node
+		return nd
 	}
 
-	n, ok := node.Children()[runes[0]]
+	n, ok := nd.Children()[runes[0]]
 	if !ok {
 		return nil
 	}
@@ -265,10 +266,10 @@ func maskruneslice(rs []rune) uint64 {
 	return m
 }
 
-func collect[T any](node *Node[T]) []string {
-	keys := make([]string, 0, node.termCount)
-	nodes := make([]*Node[T], 1, len(node.children)+1)
-	nodes[0] = node
+func collect[T any](nd *node[T]) []string {
+	keys := make([]string, 0, nd.termCount)
+	nodes := make([]*node[T], 1, len(nd.children)+1)
+	nodes[0] = nd
 	for len(nodes) > 0 {
 		i := len(nodes) - 1
 		n := nodes[i]
@@ -286,15 +287,15 @@ func collect[T any](node *Node[T]) []string {
 
 type potentialSubtree[T any] struct {
 	idx  int
-	node *Node[T]
+	node *node[T]
 }
 
-func fuzzycollect[T any](node *Node[T], partial []rune) (keys []string) {
+func fuzzycollect[T any](nd *node[T], partial []rune) (keys []string) {
 	if len(partial) == 0 {
-		return collect(node)
+		return collect(nd)
 	}
 
-	potential := []potentialSubtree[T]{{node: node, idx: 0}}
+	potential := []potentialSubtree[T]{{node: nd, idx: 0}}
 	for len(potential) > 0 {
 		i := len(potential) - 1
 		p := potential[i]
